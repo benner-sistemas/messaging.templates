f3f75e55bfece3499261a58a89f9248cb077063d
sanguedemonstro <sanguedemonstro@gmail.com>
2020-01-21 09:37:38 -0300

    Ajustes no modelo

---
+++ Benner.Corporativo.Contabilizacao.Producer/Controller/ContabilizacaoController.cs
@@ -0,0 +1,21 @@
+﻿using Benner.Corporativo.Contabilizacao.Models;
+using Benner.Messaging;
+using Benner.Messaging.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Benner.Corporativo.Contabilizacao.Producer.Controller
+{
+    [Route("api/contabilizacao")]
+    [ApiController]
+    public class ContabilizacaoController : MessagingController
+    {
+        protected override string QueueName { get => "fila-contabilizacao"; }
+
+        // POST api/contabilizacao
+        [HttpPost]
+        public ActionResult<IEnterpriseIntegrationResponse> Post([FromBody] ContabilizacaoRequest request)
+        {
+            return base.Enqueue(request as IEnterpriseIntegrationResquest);
+        }
+    }
+}
